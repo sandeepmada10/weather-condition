e2d1127ba663e15159a4ad0dad5ea05a83f42c7b
sandeep <sandeepmada10@gmail.com>
2023-04-19 12:17:36 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [time, setTime] = useState();
+  const date=new Date();
+  const current=date.getDate();
+  const year=date.getFullYear();
+
+  useEffect(() => {
+    const date=new Date();
+    const timer = setInterval(() => {
+      setTime(date.getHours()+':'+date.getMinutes()+':'+date.getSeconds());
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+//const showTime=date.getHours()+':'+date.getMinutes();
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div >
+        <input type="number" />
+        <select>
+            <option value="hi">Hi</option>
+        </select>
     </div>
+    <div className="weather">
+    <div className="left">
+        <div className="location">
+            <div className="address">
+            <img src="location.png" alt="location"  class="location-icon" height="20px" width="20px"/>
+            <p className="place">Chennai</p>
+                <img src="rain.png" alt="location" class="sunny"/></div>
+            <p class="date">{current} October {year}</p>
+            <p class="area">India</p>
+            <div class="temperature">35 ° C</div>
+        </div>
+    </div>
+    <div class="right">
+        <div class="humidity">{time}</div>
+
+    </div>
+</div>
+<div class="other">
+<div class="weather-1">
+    <div class="left">
+        <div class="location">
+            <div class="address">
+            <img src="location.png" alt="location" class="location-icon" height="20px" width="20px"/>
+            <p class="place">Bangalore</p>
+            <img src="sunny.png" alt="sunny" class="sunny"/>
+        </div>
+            <p class="date">{current} October {year}</p>
+            <p class="area">India</p>
+            <div class="temperature">30 ° C</div>
+        </div>
+    </div>
+    <div class="right">
+        <div class="humidity">{time}</div>
+        
+    </div>
+</div>
+
+<div class="weather-2">
+    <div class="left">
+        <div class="location"><div class="address">
+            <img src="location.png" alt="location" class="location-icon" height="20px" width="20px"/>
+            <p class="place">Hyderabad</p>
+            <img src="sun (1).png" alt="location" class="sunny"/></div>
+            <p class="date">{current} October {year}</p>
+            <p class="area">India</p>
+            <div class="temperature">38 ° C</div>
+        </div>
+    </div>
+    <div class="right">
+        <div class="humidity">{time}</div>
+        
+    </div>
+</div>
+</div>
+</>
   );
 }
 
